502585d3cf04ef340aca39c14dcfee5df171a1f1
jjd369 <ghana369@gmail.com>
2021-12-29 01:17:16 +0900

    console.log 제거

--- js/cart.js
+++ js/cart.js
@@ -1,18 +1,17 @@
 // 열기 버튼
 var cartHeight = $('.cart').innerHeight();
-$('.cart_btn_wrap').click(function(){
-    if(!$('.cart').hasClass('active')){
+$('.cart_btn_wrap').click(function () {
+    if (!$('.cart').hasClass('active')) {
         $('.cart').addClass('active');
         $(this).hide();
-        $('.order_content').animate({'padding-bottom':cartHeight},500);
-        console.log(cartHeight);
+        $('.order_content').animate({ 'padding-bottom': cartHeight }, 500);
     }
 })
 // 닫기 버튼
-$('.cart_close_btn').click(function(){
-    if($('.cart').hasClass('active')){
+$('.cart_close_btn').click(function () {
+    if ($('.cart').hasClass('active')) {
         $('.cart').removeClass('active');
         $('.cart_btn_wrap').fadeIn();
-        $('.order_content').animate({'padding-bottom':'5%'},500);
+        $('.order_content').animate({ 'padding-bottom': '5%' }, 500);
     }
 })
